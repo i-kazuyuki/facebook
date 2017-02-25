8ebfcd2a9ad7c07a4a7091948d1e00d4292e3a6f
kazuyuki <k,ishijima18@gmail.com>
2017-02-25 21:36:13 +0900

    letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   root'top#index'
 
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

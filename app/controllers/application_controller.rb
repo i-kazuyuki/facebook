c92f535856131a7ffca799a1f41a36a2b21d38a6
kazuyuki <k,ishijima18@gmail.com>
2017-03-07 21:26:13 +0900

    Devise revision

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #変数PERMISSIBLE_ATTRIBUTESに配列[:name]を代入
-  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
-  #変数PERMISSIBLE_ATTRIBUTESに配列[:introduction]を代入
-  PERMISSIBLE_ATTRIBUTES = %i(introduction)
+  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache introduction)
 
   # 未読の通知件数を表示
   before_action :current_notifications, if: :signed_in?

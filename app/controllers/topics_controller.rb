0715f781a8277df931a8f636c4d2d04dcfd9d1f3
kazuyuki <k,ishijima18@gmail.com>
2017-05-17 11:34:01 +0900

    topicの表示順の変更

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def index
-    @topics = Topic.all
+    @topics = Topic.all.order(created_at: :desc)
     @topic = Topic.new
   end
 
